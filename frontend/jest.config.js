0df68504d490833530c5dac110809a6424e8870c
Kiel Ed <kieledssh@gmail.com>
2023-05-03 17:27:37 +0300

    changed import in jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,35 +1,36 @@
-import nextJest from 'next/jest'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
-  dir: './',
+    dir: './',
 })
 
 const customJestConfig = {
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
 
-    '^@/public/(.*)$': '<rootDir>/public/$1',
+        '^@/public/(.*)$': '<rootDir>/public/$1',
 
-    '^__mocks__/(.*)$': '<rootDir>/__mocks__/$1',
-  },
-  setupFilesAfterEnv: ['./jest.setup.ts'],
-  clearMocks: true,
-  collectCoverage: true,
-  collectCoverageFrom: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    '!./src/**/_*.{js,jsx,ts,tsx}',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 30,
-      functions: 30,
-      lines: 30,
-      statements: 30,
+        '^__mocks__/(.*)$': '<rootDir>/__mocks__/$1',
     },
-  },
-  testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['./jest.setup.ts'],
+    clearMocks: true,
+    collectCoverage: true,
+    collectCoverageFrom: [
+        './src/**/*.{js,jsx,ts,tsx}',
+        '!./src/**/_*.{js,jsx,ts,tsx}',
+        '!**/*.d.ts',
+        '!**/node_modules/**',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 30,
+            functions: 30,
+            lines: 30,
+            statements: 30,
+        },
+    },
+    testEnvironment: 'jest-environment-jsdom',
 }
 
 export default createJestConfig(customJestConfig)
