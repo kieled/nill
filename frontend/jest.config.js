950078a3220f1bda9687d8e243beee184ef67a76
Kiel Ed <kieledssh@gmail.com>
2023-05-03 17:34:58 +0300

    changed jest configuration

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -6,31 +6,16 @@ const createJestConfig = nextJest({
 })
 
 const customJestConfig = {
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1',
-
-        '^@/public/(.*)$': '<rootDir>/public/$1',
-
-        '^__mocks__/(.*)$': '<rootDir>/__mocks__/$1',
-    },
-    setupFilesAfterEnv: ['./jest.setup.ts'],
-    clearMocks: true,
-    collectCoverage: true,
-    collectCoverageFrom: [
-        './src/**/*.{js,jsx,ts,tsx}',
-        '!./src/**/_*.{js,jsx,ts,tsx}',
-        '!**/*.d.ts',
-        '!**/node_modules/**',
-    ],
-    coverageThreshold: {
-        global: {
-            branches: 30,
-            functions: 30,
-            lines: 30,
-            statements: 30,
-        },
-    },
+    roots: ['<rootDir>'],
+    modulePaths: ['<rootDir>'],
+    moduleDirectories: ['node_modules'],
     testEnvironment: 'jest-environment-jsdom',
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/.vercel/', '<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+    transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+    transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {presets: ['next/babel']}],
+    },
+    setupFilesAfterEnv: ['@testing-library/jest-dom', '@testing-library/jest-dom/extend-expect'],
 }
 
 module.exports = createJestConfig(customJestConfig)
